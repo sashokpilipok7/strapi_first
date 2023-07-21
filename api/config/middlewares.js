91334282bd1fc1b4675187d9b8a4e5d144856e80
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-07-21 17:50:45 +0300

    feat: add strapi api

---
+++ api/config/middlewares.js
@@ -0,0 +1,24 @@
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  // {
+  //   name: "strapi::cors",
+  //   config: {
+  //     origin: "*",
+  //     headers: "*",
+  //   },
+  // },
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:3000", "http://localhost:1337"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
